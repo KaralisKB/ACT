4af8748f0dd81b0e763a5377bc06f553c2718484
KaralisKB <kristianbariss@gmail.com>
2024-09-30 20:33:35 +0100

    test

--- Backend/node/app.js
+++ Backend/node/app.js
@@ -15,5 +15,5 @@ const db = admin.firestore();
 const app = express();
 app.use(express.json());
 app.listen(3000, () => {
-    console.log("Server is running on port 3000.");
+    console.log("Server is running on port 3000."); 
 })
