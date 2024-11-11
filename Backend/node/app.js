933113819ef8c6ae11067fcbc654e2fdb021a14e
KaralisKB <kristianbariss@gmail.com>
2024-11-11 16:41:24 +0000

    cors

--- Backend/node/app.js
+++ Backend/node/app.js
@@ -21,5 +21,10 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000."); 
 })
 
-app.use(cors({origin: 'https://localhost:3000'}));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  }));
 
